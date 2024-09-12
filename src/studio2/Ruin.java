6ed5fda533367c4bf2446876215953fb77cc8660
Charlie <Charlie@localhost>
2024-09-11 21:39:16 -0500

    Hey team! Hope this works for you all

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -12,10 +12,17 @@ public class Ruin {
 		System.out.print("Please enter your win limit: ");
 		double winLimit = in.nextDouble();
 		double remainingAmount = startAmount;
-		int numberOfSimulation = 0;
-		String winLose = ""; 
+		double numberOfSimulation = 0;
+		System.out.println("How many days do you want to gamble for?");
+		int days = in.nextInt();
+		String outcome = "win";
+		double Losses = 0; 
+		double events = 1;
 		//		if math.random < win rate, true 
 		//		if win, +$1, if loss -$1  
+		for(events = 1; events < days + 1; events++) {
+			remainingAmount = startAmount;
+			numberOfSimulation = 0;
 		while (remainingAmount > 0 && remainingAmount < winLimit) {
 			numberOfSimulation++;
 			double randomNumber = (double) Math.random();
@@ -25,16 +32,27 @@ public class Ruin {
 			else {
 				remainingAmount--;
 			}
-			if (randomNumber < winChance) {
-				
+			
+			if (remainingAmount == winLimit) {
+				outcome = "Win";
 			}
-			System.out.println("Simulation " + numberOfSimulation +": " + remainingAmount);
+			if (remainingAmount == 0) {
+				outcome = "Loss";
+				Losses++;
+			}
+
+		// System.out.println("Simulation " + numberOfSimulation +": " + remainingAmount);
 		}
-		if (remainingAmount == winLimit) {
-			System.out.println("You Win");
+		System.out.println("Simulation " + events + ": " + numberOfSimulation + " " + outcome);
+
+		// if (remainingAmount == winLimit) {
+		//	System.out.println("You Win");
+		// }
+	// 	if (remainingAmount == 0) {
+		//	System.out.println("You Lose");
 		}
-		if (remainingAmount == 0) {
-			System.out.println("You Lose");
-		}
+		System.out.println("Losses: " + Losses + " Simulations: " + events);
+		System.out.println("Ruin rate from simulation: " + Math.round(Losses/events*100)/100 + "%");
+
 	}
 }
